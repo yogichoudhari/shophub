60831cc2c43acabf8365344d5b1914a3a6954d49
yogesh <yogi@gmail.com>
2022-10-31 19:06:02 +0530

    new messae gadded

--- shophub/settings.py
+++ shophub/settings.py
@@ -128,4 +128,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shophubexclusive4u@gmail.com'
-EMAIL_HOST_PASSWORD = 'nmhkdzkuqcvmzqyn'
+EMAIL_HOST_PASSWORD = 'quucuolgjpodgkjw'
