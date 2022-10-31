5ec93f9ed5ac89e0e395873ebf92ab9ee7d11f5a
yogesh <yogi@gmail.com>
2022-10-31 13:21:11 +0530

    fourth cmmi

--- shophub/urls.py
+++ shophub/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    re_path(r'^media/(?P<path>.*)$',server,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
 
 
