adab676224e680defb89a7acd16bde0ef4181258
yogesh <yogi@gmail.com>
2022-10-31 13:35:41 +0530

    setting chaned

--- shophub/settings.py
+++ shophub/settings.py
@@ -1,4 +1,4 @@
-
+import dj_database_url
 
 import os
 
@@ -74,7 +74,8 @@ DATABASES = {
         'HOST':'localhost'
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 
