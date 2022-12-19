e3b958b339b4a696ee16d0ea25dc2f2f4456443d
yogesh <yogi@gmail.com>
2022-12-19 20:06:51 +0530

    database port added with 5432 port number

--- shophub/settings.py
+++ shophub/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'USER' : 'shophub',
         'PASSWORD':'root',
         'HOST':'localhost',
-        'PORT':'5433'
+        'PORT':'5432'
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
